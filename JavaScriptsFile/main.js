d3029f044161916903afe132a816be2e9357a3c1
Sayful Islam Shakib <76785210+sayfulislamshakib@users.noreply.github.com>
2021-01-16 22:19:50 +0600

    some new things added

---
+++ JavaScriptsFile/main.js
@@ -0,0 +1,10 @@
+// Decleare variables
+var jobPaiso = true;
+var savings = 5000000;
+
+if (jobPaiso == true && savings > 20000) {
+  console.log("Asho tomar jonno bia khuji!")
+}
+else { 
+  console.log("Tomar to taka e nai mia!");
+}
