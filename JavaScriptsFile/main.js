5845aa38c7655578ac2f196bdf2772b5f910590b
Sayful Islam Shakib <76785210+sayfulislamshakib@users.noreply.github.com>
2021-01-17 22:07:01 +0600

    So many changes are made

--- JavaScriptsFile/main.js
+++ JavaScriptsFile/main.js
@@ -1,10 +1,35 @@
+//First time learnign the JavaScript
 // Decleare variables
-var jobPaiso = true;
-var savings = 5000000;
 
-if (jobPaiso == true && savings > 20000) {
-  console.log("Asho tomar jonno bia khuji!")
+// var jobPaiso = true;
+// var savings = 5000000;
+// //If, else, else if
+// if (jobPaiso == true && savings > 20000) {
+//   console.log("Asho tomar jonno bia khuji!")
+// }
+// else {
+//   console.log("Tomar to taka e nai mia!");
+// }
+// var age = [15, 10, 12, 13]
+// age.unshift(100);
+// console.log(age)
+// var numbers = [1,2,3,4,5];
+
+// var result = numbers.slice(1,3);
+
+// console.log(result)
+// var num = 0;
+// while (num < 15) {
+//   console.log(num);
+//   num++;
+// }
+// var nums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// for (var i = 0; i < nums.length; i++) {
+//   var element = nums[i];
+//   console.log(element)
+// }
+function myFunction() {
+	console.log("Hi I am a web devoloper!");
+	console.log("Now I am learning JavaScript!");
 }
-else { 
-  console.log("Tomar to taka e nai mia!");
-}
+myFunction();
