4a88a27b0897749483f1e37c196c075a9e75904b
Sayful Islam Shakib <76785210+sayfulislamshakib@users.noreply.github.com>
2021-01-19 12:46:13 +0600

    fibonacchi law

---
+++ JavaScriptsFile/fibonacci.js
@@ -0,0 +1,10 @@
+//function decleared
+function fibonacci(n) {
+	let fibo = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+	return fibo;
+}
+let result = fibonacci(12);
+console.log(result);
